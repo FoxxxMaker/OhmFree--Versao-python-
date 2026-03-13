0a36224c3b204b8ad795a2eb7f9d62945dda0b78
Levi Soares <levisoaresp.alho.8c.m@gmail.com>
2026-03-13 20:55:41 -0300

    adiciona arquivos Back.py e front.py

---
+++ Back.py
@@ -0,0 +1,5 @@
+# Lei de Ohm: V = R * I
+
+# cores: preto: 0, marrom: 1, vermelho: 2, laranja: 3, amarelo: 4, verde: 5, azul: 6, violeta: 7, cinza: 8, branco: 9
+
+# 1ª faixa: 1º dígito, 2ª faixa: 2º dígito, 3ª faixa: multiplicador, 4ª faixa: tolerância (opcional)
