d1382c5ff233bafc913fbecea188b1ed99fbf955
Levi Soares <levisoaresp.alho.8c.m@gmail.com>
2026-03-13 21:01:59 -0300

    adiciona função Resistor para cálculo de resistência com base nas cores

--- Back.py
+++ Back.py
@@ -3,3 +3,21 @@
 # cores: preto: 0, marrom: 1, vermelho: 2, laranja: 3, amarelo: 4, verde: 5, azul: 6, violeta: 7, cinza: 8, branco: 9
 
 # 1ª faixa: 1º dígito, 2ª faixa: 2º dígito, 3ª faixa: multiplicador, 4ª faixa: tolerância (opcional)
+
+def Resistor(cor1, cor2, cor3, cor4=None):
+    
+    Cores = {
+        "preto": 0,
+        "marrom": 1,
+        "vermelho": 2,
+        "laranja": 3,
+        "amarelo": 4,
+        "verde": 5,
+        "azul": 6,
+        "violeta": 7,
+        "cinza": 8,
+        "branco": 9
+    }
+
+    R = (Cores[cor1] * 10 + Cores[cor2]) * (10 ** Cores[cor3])
+    return R
